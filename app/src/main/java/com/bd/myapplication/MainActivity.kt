e86688d86150b03d35f85df67aaea0368b93cb60
Burak Dizlek <dizlekburak@gmail.com>
2019-06-18 23:32:10 +0300

    all project.

---
+++ app/src/main/java/com/bd/myapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.bd.myapplication
+
+import android.os.Build
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
+
+    }
+}
